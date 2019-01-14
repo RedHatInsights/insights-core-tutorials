eb943a15cf4e6b55d60b6420057e0b22ca008226
Lloyd Huett <lhuett@redhat.com>
2019-01-14 14:36:25 -0600

    Corrected some issues with conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 import sys
 import os
-import examples
+import insights_examples
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../examples'))
+sys.path.insert(0, os.path.abspath('../insights_examples'))
 
 # -- General configuration ------------------------------------------------
 
