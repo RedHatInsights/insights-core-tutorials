878a5da94f72c8cee604394cf11cfc86eb595eec
Lloyd Huett <lhuett@redhat.com>
2019-01-22 16:07:53 -0600

    Corrected issue in build.sh and .travis.yml

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 
 set -ev
 
-if [ "`python -V 2>&1`" == "Python 2.6.9" ]; then 
-    cp .collections.py /home/travis/virtualenv/python2.6.9/lib/python2.6/collections.py
-fi
+mkdir -p mycomponents
 
-py.test
+pytest
 
 flake8;
 sphinx-build -W -b html -qa -E docs docs/_build/html;
