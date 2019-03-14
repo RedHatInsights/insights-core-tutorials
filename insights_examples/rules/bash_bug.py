a26124daa660bdb1069622914e64b09f15190949
Son Nguyen <songuyen@redhat.com>
2019-03-14 15:39:50 -0500

    Change to use ERROR_KEY_BASH_BUG_NOT_PRESENT and ERROR_KEY_BASH_BUG_PRESENT in bash_bug rule

--- insights_examples/rules/bash_bug.py
+++ insights_examples/rules/bash_bug.py
@@ -1,9 +1,12 @@
 from insights import rule, make_pass, make_fail
 from insights.parsers.installed_rpms import InstalledRpm, InstalledRpms
 
+ERROR_KEY_BASH_BUG_PRESENT = "BASH_BUG_PRESENT"
+ERROR_KEY_BASH_BUG_NOT_PRESENT = "BASH_BUG_NOT_PRESENT"
+
 CONTENT = {
-    "BASH_BUG_PRESENT": "Bash bug found! Version: {{bash}}",
-    "BASH_BUG_NOT_PRESENT": "Bash bug not found: {{bash}}."
+    ERROR_KEY_BASH_BUG_PRESENT: "Bash bug found! Version: {{bash}}",
+    ERROR_KEY_BASH_BUG_NOT_PRESENT: "Bash bug not found: {{bash}}."
 }
 
 
@@ -13,6 +16,6 @@ def check_bash_bug(rpms):
     fix_version = InstalledRpm.from_package('bash-4.4.18-1.any')
     current_version = rpms.get_max('bash')
     if bug_version <= current_version < fix_version:
-        return make_fail('BASH_BUG_PRESENT', bash=current_version.nvr)
+        return make_fail(ERROR_KEY_BASH_BUG_PRESENT, bash=current_version.nvr)
     else:
-        return make_pass('BASH_BUG_NOT_PRESENT', bash=current_version.nvr)
+        return make_pass(ERROR_KEY_BASH_BUG_NOT_PRESENT, bash=current_version.nvr)
